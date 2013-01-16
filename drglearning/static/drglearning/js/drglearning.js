d5a346097c42d0e6fee8ce19244ee846c1176534
Javier de la Rosa <versae@gmail.com>
2013-01-16 16:38:55 -0500

    Place the js files in the right location

---
+++ drglearning/static/drglearning/js/drglearning.js
@@ -0,0 +1,34 @@
+(function ($) {
+  init = function () {
+    var drglearningHost = $("#drglearning-host").val();
+        urlToEmbed = $("#career-url").val(),
+        iframe = $("#career-iframe"),
+        title = $("#career-title");
+        playerCode = $("#player-code").val();
+
+    $("a.career-id").each(function (e) {
+      var $this = $(this);
+      $this.on("click", function (elto) {
+        var careerId = $this.data("career-id"),
+            src = urlToEmbed + careerId +"&import="+ document.location.origin +"#"+ encodeURIComponent(document.location.href);
+        title.text($this.text());
+        iframe.attr("src", src);
+
+        XD.receiveMessage(function (message) {
+          if (message.origin !== drglearningHost) {
+            return;
+          }
+          if (message.data == "ready") {
+            args = {"action": "importPlayer", "params": {"playerCode": playerCode}};
+            XD.postMessage(args, src, frames[0]);
+          }
+        }, drglearningHost);
+
+        return false;
+      });
+    });
+
+    $("a.career-id:first").click();
+  };
+  $(document).ready(init);
+})(jQuery);
