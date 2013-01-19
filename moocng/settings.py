17b45f39c142491d5eb2f215d84462f426fea672
Javier de la Rosa <versae@gmail.com>
2013-01-18 21:46:18 -0500

    Add links to default toolbars for TinyMCE

--- moocng/settings.py
+++ moocng/settings.py
@@ -283,7 +283,7 @@ FIXTURE_DIRS = (
 TINYMCE_DEFAULT_CONFIG = {
     'theme': 'advanced',
     'theme_advanced_toolbar_location': 'top',
-    'theme_advanced_buttons1': 'bold,italic,underline,strikethrough,separator,undo,redo,separator,cleanup,separator,bullist,numlist',
+    'theme_advanced_buttons1': 'bold,italic,underline,strikethrough,separator,undo,redo,separator,cleanup,separator,bullist,numlist,separator,link',
     'theme_advanced_buttons2' : '',
     'theme_advanced_buttons3' : '',
     }
